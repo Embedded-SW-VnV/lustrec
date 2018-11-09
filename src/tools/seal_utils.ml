a5dc55ca644f53ba05f82da6de353b14d96ea6b8
ploc <ploc@garoche.net>
2018-11-09 07:43:09 +0100

    Restructuring code in SEAL

--- src/tools/seal_utils.ml
+++ src/tools/seal_utils.ml
@@ -2,91 +2,102 @@ open Lustre_types
 open Utils
    
 let report = Log.report ~plugin:"seal"
-           
+
+			
+			
+(******************************************************************************)
+(* Computing a slice of a node, selecting only some variables, based on       *)
+(* their COI (cone of influence)                                              *)
+(******************************************************************************)
+
+(* Basic functions to search into nodes. Could be moved to corelang eventually *)
+let is_variable nd vid = 
+  List.exists
+    (fun v -> v.var_id = vid)
+    nd.node_locals
+  
+let find_variable nd vid = 
+  List.find
+    (fun v -> v.var_id = vid)
+    nd.node_locals
+
+(* Returns the vars required to compute v. 
+   Memories are specifically identified. *)
+let coi_var deps nd v =
+  let vname = v.var_id in
+  let sliced_deps =
+    Causality.slice_graph deps vname
+  in
+  (* Format.eprintf "sliced graph for %a: %a@."
+   *   Printers.pp_var v
+   *   Causality.pp_dep_graph sliced_deps; *)
+  let vset, memset =
+    IdentDepGraph.fold_vertex
+      (fun vname (vset,memset)  ->
+       if Causality.ExprDep.is_read_var vname
+       then
+         let vname' = String.sub vname 1 (-1 + String.length vname) in
+         if is_variable nd vname' then
+           ISet.add vname' vset,
+           ISet.add vname' memset
+         else
+           vset, memset
+       else
+         ISet.add vname vset, memset
+      )
+      sliced_deps
+      (ISet.singleton vname, ISet.empty)
+  in
+  report ~level:3 (fun fmt -> Format.fprintf fmt "COI of var %s: (%a // %a)@."
+					     v.var_id
+					     (fprintf_list ~sep:"," Format.pp_print_string) (ISet.elements vset)
+					     (fprintf_list ~sep:"," Format.pp_print_string) (ISet.elements memset)
+		  )  ;
+  vset, memset
+
+	  
+(* Computes the variables required to compute vl. Variables /seen/ do not need
+     to be computed *)
+let rec coi_vars deps nd vl seen =
+  let coi_vars = coi_vars deps nd in
+  List.fold_left
+    (fun accu v ->
+     let vset, memset = coi_var deps nd v in
+     (* We handle the new mems discovered in the coi *)
+     let memset =
+       ISet.filter (
+           fun vid ->
+           not
+             (List.exists
+                (fun v -> v.var_id = vid)
+                vl
+             ) 
+         ) memset
+     in
+     let memset_vars = 
+       ISet.fold (
+           fun vid accu ->
+           (find_variable nd vid)::accu
+         ) memset [] 
+     in
+     let vset' =
+       coi_vars memset_vars (vl@seen)
+     in
+     ISet.union accu (ISet.union vset vset')
+    )
+    ISet.empty
+    (List.filter
+       (fun v -> not (List.mem v seen))
+       vl
+    )
+    
+    
+(* compute the coi of vars_to_keeps in node nd *)
 let compute_sliced_vars vars_to_keep deps nd =
-  let is_variable vid = 
-    List.exists
-      (fun v -> v.var_id = vid)
-      nd.node_locals
-  in
-  let find_variable vid = 
-    List.find
-      (fun v -> v.var_id = vid)
-      nd.node_locals
-  in  
+  ISet.elements (coi_vars deps nd vars_to_keep []) 
 
-  (* Returns the vars required to compute v. 
-     Memories are specifically identified. *)
-  let coi_var v =
-    let vname = v.var_id in
-    let sliced_deps =
-      Causality.slice_graph deps vname
-    in
-    (* Format.eprintf "sliced graph for %a: %a@."
-     *   Printers.pp_var v
-     *   Causality.pp_dep_graph sliced_deps; *)
-    let vset, memset =
-      IdentDepGraph.fold_vertex
-        (fun vname (vset,memset)  ->
-          if Causality.ExprDep.is_read_var vname
-          then
-            let vname' = String.sub vname 1 (-1 + String.length vname) in
-            if is_variable vname' then
-              ISet.add vname' vset,
-              ISet.add vname' memset
-            else
-              vset, memset
-          else
-            ISet.add vname vset, memset
-        )
-        sliced_deps
-        (ISet.singleton vname, ISet.empty)
-    in
-    report ~level:3 (fun fmt -> Format.fprintf fmt "COI of var %s: (%a // %a)@."
-                                    v.var_id
-                                    (fprintf_list ~sep:"," Format.pp_print_string) (ISet.elements vset)
-                                    (fprintf_list ~sep:"," Format.pp_print_string) (ISet.elements memset)
-      )  ;
-    vset, memset
-  in
 
-  (* Computes the variables required to compute
-     vl. Variables /seen/ do not need to 
-     be computed *) 
-  let rec coi_vars vl seen = 
-    List.fold_left
-      (fun accu v ->
-        let vset, memset = coi_var v in
-        (* We handle the new mems 
-           discovered in  the coi *)
-        let memset =
-          ISet.filter (
-              fun vid ->
-              not
-                (List.exists
-                   (fun v -> v.var_id = vid)
-                   vl
-                ) 
-            ) memset
-        in
-        let memset_vars = 
-          ISet.fold (
-              fun vid accu ->
-              (find_variable vid)::accu
-            ) memset [] 
-        in
-        let vset' =
-          coi_vars memset_vars (vl@seen)
-        in
-        ISet.union accu (ISet.union vset vset')
-      )
-      ISet.empty
-      (List.filter
-         (fun v -> not (List.mem v seen))
-         vl
-      )
-  in
-  ISet.elements (coi_vars vars_to_keep []) 
+
 
 
   (* If existing outputs are included in vars_to_keep, just slice the content.
