81e2ad2fd7e1fe069d2cc8d1d51916384d1da7bf
Guillaume DAVY <guillaume@davy.xyz>
2019-02-12 15:02:15 +0100

    Ada: Remove useless comment

--- src/backends/Ada/ada_backend_ads.ml
+++ src/backends/Ada/ada_backend_ads.ml
@@ -128,36 +128,3 @@ let print fmt machine =
     (*(Utils.fprintf_list ~sep:"@," pp_var_decl) machine.mmemory*)
 
 end
-
-(*
-package Example is
-     type Number is range 1 .. 11;
-     procedure Print_and_Increment (j: in out Number);
-end Example;
-
-Package body (example.adb)
-
-with Ada.Text_IO;
-package body Example is
-
-  i : Number := Number'First;
-
-  procedure Print_and_Increment (j: in out Number) is
-
-    function Next (k: in Number) return Number is
-    begin
-      return k + 1;
-    end Next;
-
-  begin
-    Ada.Text_IO.Put_Line ( "The total is: " & Number'Image(j) );
-    j := Next (j);
-  end Print_and_Increment;
-
--- package initialization executed when the package is elaborated
-begin
-  while i < Number'Last loop
-    Print_and_Increment (i);
-  end loop;
-end Example;
-*)
