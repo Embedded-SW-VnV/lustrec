2ddbde7dc3cd3206ac004d134780b98114d6d321
Guillaume DAVY <guillaume@davy.xyz>
2019-02-14 14:42:49 +0100

    Ada: Last correction was incorrect

--- src/backends/Ada/ada_backend_adb.ml
+++ src/backends/Ada/ada_backend_adb.ml
@@ -53,7 +53,7 @@ let pp_step_definition fmt m = pp_procedure_definition
       pp_var_decl
       (pp_machine_instr m)
       fmt
-      ([], m.mstep)
+      ([], m.mstep.step_instrs)
 
 (** Print the definition of the reset procedure from a machine.
    @param fmt the formater to print on
