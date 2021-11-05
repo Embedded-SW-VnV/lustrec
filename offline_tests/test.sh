7cc1f808a318913c6f2b99d294d275961224b53f
Lélio Brun <lelio.brun@isae-supaero.fr>
2021-11-05 12:38:09 +0100

    add timeout to tests

--- offline_tests/test.sh
+++ offline_tests/test.sh
@@ -9,7 +9,7 @@ LUSTREC=$1
 LUS_FILES=$2
 
 PROVERS=alt-ergo,z3,cvc4
-TIMEOUT="${TIMEOUT:-30}"
+TIMEOUT="${TIMEOUT:-60}"
 JOBS="${JOBS:-16}"
 FRAMA_C_ARGS="-wp -wp-model ref,real -wp-prover $PROVERS -wp-run-all-provers\
     -wp-timeout $TIMEOUT -wp-par $JOBS"
@@ -57,30 +57,50 @@ verif() {
     N=0
     OK=0
     KO=0
+    TIMEOUTS=()
+    FAILED=()
     for f in *.c
     do
         N=$(( N + 1 ))
         printf "%3.0f%% ${normal}%-${S}s" "$(((100 * N)/M))" "$f"
-        if $FRAMA_C $FRAMA_C_ARGS "$f" > /tmp/log; then
+        timeout 1h $FRAMA_C $FRAMA_C_ARGS "$f" > /tmp/log; ec=$?
+        if [ $ec -eq 0 ]; then
             sed -n '/Proved goals/{N;N;N;N;p;q}' /tmp/log > "$f".log
             OK=$(( OK + 1 ))
             LOG=$(<"$f".log)
             CHECK="${green}OK\n  $LOG\n${normal}"
         else
+            if [ $ec -eq 124 ]; then
+                ERR="TIMEOUT!"
+                TIMEOUTS+=("$f")
+            else
+                ERR=$(</tmp/log)
+                FAILED+=("$f")
+            fi
             KO=$(( KO + 1 ))
-            ERR=$(</tmp/log)
             CHECK="${red}KO\n  $ERR\n${normal}"
         fi
         printf " %b\n" "${CHECK}"
     done
     printf "\n${normal}OK: ${green}%d${normal} (${red}%d${normal}) / %d\n\n"\
         "${OK}" "${KO}" "${M}"
+    printf "\n${bold}Timeouts:${normal}\n"
+    for f in "${TIMEOUTS[@]}"
+    do
+        printf "  %s\n" "$f"
+    done
+    printf "\n${bold}Failed:${normal}\n"
+    for f in "${FAILED[@]}"
+    do
+        printf "  %s\n" "$f"
+    done
+
 }
 
 printf "\n${bold}Ignored tests:${normal}\n"
 for f in $IGNORED
 do
-    printf "  %s\n" $f
+    printf "  %s\n" "$f"
 done
 
 compile
