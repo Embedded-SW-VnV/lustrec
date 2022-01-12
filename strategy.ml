f486c47afe545a59269f9dc296243c9639126924
Lélio Brun <lelio.brun@isae-supaero.fr>
2022-01-12 15:32:47 +0100

    more robust strategy, able to handle footprint lemmas

--- strategy.ml
+++ strategy.ml
@@ -31,6 +31,11 @@ let select_g (g: Lang.F.pred) : Tactical.selection =
   let open Tactical in
   Clause (Goal g)
 
+(* tactical selection of an hypothesis *)
+let select_h (h: Conditions.step) : Tactical.selection =
+  let open Tactical in
+  Clause (Step h)
+
 (* get variables and predicate under existential binder *)
 let existential_vars_g (g: Lang.F.pred) : Lang.F.var list * Lang.F.term =
   let open Lang.F in
@@ -51,6 +56,62 @@ let is_var (v: Lang.F.var) (t: Lang.F.term) : bool =
   | Var w -> v = w
   | _ -> false
 
+exception Found of Tactical.selection list
+
+(* a function to search a transition call in the hypotheses *)
+(*  that can be unified with a term *)
+let rec unify
+    (push: Strategy.strategy -> unit)
+    (sequent: Conditions.sequent)
+    (vars: Lang.F.var list)
+    (term: Lang.F.term) : unit =
+  let hyps = fst sequent in
+  match Repr.term term with
+  | Call (p, ts) ->
+    (* get the index of the transition under the binder *)
+    begin match is_transition p with
+      | Some n ->
+        (* find the indexes of the existential variables *)
+        let idxs = List.map (fun v -> find_index (is_var v) ts) vars in
+        (* we search the hypotheses to find the corresponding terms *)
+        Conditions.iter (fun step -> match step.condition with
+            | Have p ->
+              (* in case of footprint lemma : unfold the hypothesis *)
+              begin match Repr.pred p with
+                | Call (p, _) when is_transition_n (n + 1) p ->
+                  push (Auto.definition (select_h step))
+                | _ -> ()
+              end;
+              (* a function to find an hypothesis *)
+              let find_hyp term =
+                match Repr.term term with
+                | Call (p, ts) when is_transition_n n p ->
+                  let witnesses = List.map (fun i ->
+                      let t = List.nth ts i in
+                      Strategy.select_e sequent t) idxs
+                  in
+                  raise (Found witnesses)
+                | _ -> ()
+              in
+              let p = Lang.F.e_prop p in
+              (* is the whole hypothesis the transition call? *)
+              find_hyp p;
+              (* otherwise, explore sub-terms to find it *)
+              Lang.F.lc_iter find_hyp p
+            | _ -> ()) hyps
+      | None -> ()
+    end
+
+  | HigherOrder ->
+    let pool = Lang.F.pool () in
+    let fv = Lang.F.e_vars term in
+    List.iter (Lang.F.add_var pool) fv;
+    let qvars, term = Lang.F.e_open ~forall:false ~lambda:false ~pool term in
+    unify push sequent vars term
+
+  | _ ->
+    Lang.F.lc_iter (unify push sequent vars) term
+
 class lustrec : Strategy.heuristic =
   object
     method id = "LustreC" (* required, must be unique *)
@@ -58,7 +119,7 @@ class lustrec : Strategy.heuristic =
     method descr = "Custom goal transformations" (* idem *)
 
     method search (push: Strategy.strategy -> unit) (sequent: Conditions.sequent) : unit =
-      let hyps, goal = sequent in
+      let goal = snd sequent in
       match Repr.pred goal with
       (* if the goal is only a transition or memory pack call, unfold it *)
       | Call (p, _) when is_transition p <> None || is_pack p ->
@@ -68,41 +129,8 @@ class lustrec : Strategy.heuristic =
       | HigherOrder ->
         (* get the existential variables *)
         let vars, g = existential_vars_g goal in
-        let exception Found of Tactical.selection list in
-        (* a function to search a transition call in the hypotheses *)
-        (*  that can be unified with a term *)
-        let unify term =
-          match Repr.term term with
-          | Call (p, ts) ->
-            (* get the index of the transition under the binder *)
-            begin match is_transition p with
-              | Some n ->
-                (* find the indexes of the existential variables *)
-                let idxs = List.map (fun v -> find_index (is_var v) ts) vars in
-                (* we search the hypotheses to find the corresponding terms *)
-                Conditions.iter (fun step -> match step.condition with
-                    | Have p ->
-                      begin match Repr.pred p with
-                        | Call (p, ts) when is_transition_n n p ->
-                          let witnesses = List.map (fun i ->
-                              let t = List.nth ts i in
-                              Strategy.select_e sequent t) idxs
-                          in
-                          raise (Found witnesses)
-
-                        | _ -> ()
-                      end
-                    | _ -> ()) hyps
-              | None -> ()
-            end
-          | _ -> ()
-        in
-        begin try
-            (* is the whole term a transition call ? *)
-            unify g;
-            (* otherwise, explore sub-terms to find a transition call *)
-            Lang.F.lc_iter unify g
-          with
+        (* find the transition call under binders *)
+        begin try unify push sequent vars g with
           | Found witnesses ->
             (* instantiate the variables with found (rev ??) witnesses *)
             push (Auto.instance (select_g goal) (List.rev witnesses));
