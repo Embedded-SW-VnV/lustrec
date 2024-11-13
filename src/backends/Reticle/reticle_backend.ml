b63160dc07cbd0d6e2fcdc767a382f15022652c6
ploc <ploc@garoche.net>
2024-11-13 18:00:07 +0100

    missing reticle file

---
+++ src/backends/Reticle/reticle_backend.ml
@@ -0,0 +1,8 @@
+(* Reticle backend
+
+   - based on EMF backend
+   - TODO add extra normalisation
+   - TODO adds export of the dependency graph for variables
+*)
+
+let translate = EMF_backend.translate
